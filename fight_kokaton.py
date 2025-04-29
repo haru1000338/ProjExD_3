67f353dccf11acab468a60dcd19dd617ed895209
c0a24102bb <h.m1000338@gmail.com>
2025-04-29 14:29:38 +0900

    爆弾とビームの衝突判定

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -158,18 +158,26 @@ def main():
                 beam = Beam(bird)            
         screen.blit(bg_img, [0, 0])
         
-        if bird.rct.colliderect(bomb.rct):
-            # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-            bird.change_img(8, screen)
-            pg.display.update()
-            time.sleep(1)
-            return
+        if bomb is not None:
+            if bird.rct.colliderect(bomb.rct):
+                bird.change_img(8, screen)
+                pg.display.update()
+                time.sleep(1)
+                return
+
+        if beam is not None:
+            if bomb is not None:  # bombがNoneでないことを確認
+                if beam.rct.colliderect(bomb.rct):
+                    # ビームと爆弾が衝突したら，ビームを消す
+                    beam = None  # ビームを消す
+                    bomb = None  # 爆弾を消す
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
         if beam is not None:
             beam.update(screen)
-        bomb.update(screen)
+        if bomb is not None:
+            bomb.update(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
